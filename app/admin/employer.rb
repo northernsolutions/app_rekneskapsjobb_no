fa04cc41464f09bf960fa7025c8abec247fd0717
Jacob Hedengren <=>
2017-01-22 21:34:11 +0100

    set up navbar and links, added some styling

--- app/admin/employer.rb
+++ app/admin/employer.rb
@@ -2,4 +2,28 @@ ActiveAdmin.register Employer do
 
   permit_params :profile, :name, :about, :vision, :website, :facebook_link, :twitter_link, :youtube_link, :instagram_link, :google_plus_link, :linkedin_link, :published_at
 
+  action_item :view, only: :show do
+    link_to 'View on site', employer_path(employer) if employer.published_at?
+  end
+
+  action_item :publish, only: :show do
+    link_to "Publish", publish_admin_employer_path(employer), method: :put if !employer.published_at?
+  end
+
+  action_item :unpublish, only: :show do
+    link_to "Unpublish", unpublish_admin_employer_path(employer), method: :put if employer.published_at?
+  end
+
+  member_action :publish, method: :put do
+    employer = Employer.find(params[:id])
+    employer.update(published_at: Time.zone.now)
+    redirect_to admin_employer_path(employer)
+  end
+
+  member_action :unpublish, method: :put do
+    employer = Employer.find(params[:id])
+    employer.update(published_at: nil)
+    redirect_to admin_employer_path(employer)
+  end
+
 end
--- app/admin/job.rb
+++ app/admin/job.rb
@@ -2,4 +2,28 @@ ActiveAdmin.register Job do
 
   permit_params :exclusive, :employer_id, :job_category_id, :job_type_id, :title, :content, :location, :apply_date, :apply_info, :contact_info, :published_at
 
+  action_item :view, only: :show do
+    link_to 'View on site', job_path(job) if job.published_at?
+  end
+
+  action_item :publish, only: :show do
+    link_to "Publish", publish_admin_job_path(job), method: :put if !job.published_at?
+  end
+
+  action_item :unpublish, only: :show do
+    link_to "Unpublish", unpublish_admin_job_path(job), method: :put if job.published_at?
+  end
+
+  member_action :publish, method: :put do
+    job = Job.find(params[:id])
+    job.update(published_at: Time.zone.now)
+    redirect_to admin_job_path(job)
+  end
+
+  member_action :unpublish, method: :put do
+    job = Job.find(params[:id])
+    job.update(published_at: nil)
+    redirect_to admin_job_path(job)
+  end
+
 end
