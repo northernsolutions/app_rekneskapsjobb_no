02053392bab702e5e21f0e62dcfa55db2e605ea6
Jacob Hedengren <=>
2017-01-22 20:52:06 +0100

    added jobs, employers, for_employers, contact and about controllers with actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :jobs
+  resources :employers
+  resources :for_employers
+  resources :contact
+  resources :about
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'landing#index'
